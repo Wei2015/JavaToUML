87215ec0eca73a326ef182d0454960763e5afecc
Wei Yao <wei.yao@sjsu.edu>
2017-02-18 12:55:53 -0800

    Refractor class names and delete some wrapper classes.

--- src/ClassWrapper.java
+++ src/ClassWrapper.java
@@ -3,15 +3,39 @@
  * One object of this class wrap all information about a single class
  */
 
-import java.util.HashMap;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
 import java.util.ArrayList;
 
 public class ClassWrapper {
 
     private String className;
 
-    private ArrayList<MethodWrapper> methods;
+    private ArrayList<String> methods;
 
-    private ArrayList<AttributeWrapper> attributes;
+    private ArrayList<String> attributes;
+
+
+    private String attributeName;
+
+    private String attributeType;
+
+    private boolean attributeIsPublic;
+
+    private String methodName;
+
+    private String returnType;
+
+    private String[] parameters;
+
+
 
 }

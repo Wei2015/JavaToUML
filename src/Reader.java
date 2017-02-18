07997cf2b43002b776df58adabfc6a6a122de7ba
Wei Yao <wei.yao@sjsu.edu>
2017-02-18 12:54:32 -0800

    Refractor class names and delete some wrapper classes.

--- src/Reader.java
+++ src/Reader.java
@@ -6,89 +6,111 @@ import com.github.javaparser.*;
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.BodyDeclaration;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.TypeDeclaration;
-import sun.tools.java.ClassDeclaration;
+import com.github.javaparser.ast.body.*;
 
 
-public class ReadFiles {
+public class Reader {
 
     private String path;
 
     private HashSet<String> classSet;
 
-    public ReadFiles(String path) {
+    private StringBuilder result;
+
+
+    public Reader(String path) {
 
         this.path = path;
         classSet = new HashSet<>();
+        result = new StringBuilder();
     }
 
-    /*This method will print all files' name from a given path
-    * */
-    public String open(){
+    /*
+     *This method will print all files' name from a given path
+     */
+    public String readDirectory() {
 
         File directory = new File(path);
 
-        StringBuilder result = new StringBuilder();
 
         File[] fList = directory.listFiles();
 
         //Collect all file names into a set, they are either class name or interface names.
-        for (File file : fList){
-            if (file.isFile()){
+        for (File file : fList) {
+            if (file.isFile()) {
                 //add class name into a set
                 classSet.add(file.getName());
             }
         }
+
         //For each java file, perform parsing.
-        for (File file :fList) {
+        for (File file : fList) {
             if (file.isFile()) {
-                parseOneFile(file, result);
+
+                parseFile(file);
             }
         }
 
-        return result.toString();
+        //convert StringBuilder to String for return
+
+        return getResult();
 
     }
-    //one java file is one class
-    private void parseOneFile(File file, StringBuilder content) {
+
+    /*
+     * Extract file content to StringBuilder
+     */
+    private void parseFile(File file) {
         CompilationUnit cu = new CompilationUnit();
         //parse file
         try {
-           cu = JavaParser.parse(file);
+            cu = JavaParser.parse(file);
         } catch (FileNotFoundException e) {
             System.out.print(e);
         }
 
-        //get class name
+        //add Class name to result
         TypeDeclaration type = cu.getType(0);
-
         String className = type.getName().getIdentifier();
 
-        System.out.println(className);
+
+
+        //add Attributes, distinguish composite class and other variable
 
         NodeList<BodyDeclaration> members = type.getMembers();
-        for (BodyDeclaration m: members) {
+        for (BodyDeclaration m : members) {
             if (m instanceof MethodDeclaration && ((MethodDeclaration) m).isPublic()) {
                 String methodName = ((MethodDeclaration) m).getName().toString();
 
                 String returnType = ((MethodDeclaration) m).getType().toString();
+                if (returnType != "void") {
+                    System.out.println(methodName + "; " + returnType);
+                }
 
-                ((MethodDeclaration) m).getTypeParameters();
+          
+
+                for (Parameter p: ((MethodDeclaration) m).getParameters() ){
+                    String parameter = p.getName().getIdentifier();
+                    System.out.println(parameter);
+                };
             }
-
         }
 
 
-
-
+        //add Methods info
 
 
     }
 
 
+    /*
+     * format result to plantUML readable string
+     */
+    private String getResult() {
+
+
+        return result.toString();
+    }
 }
 
 

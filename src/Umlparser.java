8820f5350402310550a74d96f57d27315dd98547
Wei Yao <wei.yao@sjsu.edu>
2017-02-18 13:01:32 -0800

    Refractor class names and delete some wrapper classes.

--- src/Umlparser.java
+++ src/Umlparser.java
@@ -1,10 +1,18 @@
+
+
 public class Umlparser {
 
     public static void main(String[] args) {
 
-        ReadFiles newDir = new ReadFiles(args[0]);
 
-        System.out.print(newDir.open());
+        //Read files from the args[0] into string
+
+        Reader newDir = new Reader(args[0]);
+        String result = newDir.readDirectory();
+
+
+
+
 
     }
 }

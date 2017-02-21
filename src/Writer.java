c31c109b02ab4c5c6432334b6802b64b339b0e08
Wei Yao <wei.yao@sjsu.edu>
2017-02-21 11:28:27 -0800

    Writer.java can write string content into an svg file.

---
+++ src/Writer.java
@@ -0,0 +1,45 @@
+/**
+ * This class is taking string input in PlantUML format and write into a svg file in the given file name.
+ */
+
+import net.sourceforge.plantuml.*;
+import java.io.*;
+public class Writer {
+
+    public static void draw(String source, String fileName) {
+
+        source = source.replaceAll ("^[ |\t]*\n$", "").trim();
+
+
+        SourceStringReader reader = new SourceStringReader(source);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        FileFormatOption f_option = new FileFormatOption(FileFormat.SVG);
+        String svg="";
+
+        try {
+            reader.generateImage(os,f_option);
+            svg = new String(os.toByteArray());
+        } catch (IOException e) {
+            System.out.println("could not generate the image");
+        }
+
+        try {
+            File file = new File(fileName);
+            file.createNewFile();
+            FileWriter fstream = new FileWriter(file);
+            BufferedWriter wr = new BufferedWriter(fstream);
+            wr.write(svg);
+            wr.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try {
+            os.close();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+    }
+
+}
